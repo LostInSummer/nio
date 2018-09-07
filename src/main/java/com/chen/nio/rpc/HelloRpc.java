9fd46c1d0e169a258d2a0cc3b86792d654125861
75723 <75723@192.168.88.1>
2018-09-07 09:44:07 +0800

    提交netty事例，简单的helloworld和netty模拟实现rpc

---
+++ src/main/java/com/chen/nio/rpc/HelloRpc.java
@@ -0,0 +1,7 @@
+package com.chen.nio.rpc;
+
+public interface HelloRpc {
+
+	String hello(String name);
+
+}
---
+++ src/main/java/com/chen/nio/rpc/Main.java
@@ -0,0 +1,11 @@
+package com.chen.nio.rpc;
+
+public class Main {
+
+	public static void main(String[] args) {
+		HelloRpc helloRpc = new HelloRpcImpl();
+		helloRpc = RPCProxy.create(helloRpc);
+		System.err.println(helloRpc.hello("rpc"));
+	}
+
+}
